ab3de91906b13ed114ab2d406d8025b065bf3b21
Herbert Bergauer <hbergaue@srv-b1b07-20-01.cern.ch>
2023-10-11 12:57:36 +0200

    inserted create_module function

--- ugt_fwtools/synth_1_module.py
+++ ugt_fwtools/synth_1_module.py
@@ -5,7 +5,7 @@ import logging
 import os
 import subprocess
 from . import utils
-from .synthesis import implement_module, show_screen_sessions, start_screen_session
+from .synthesis import create_module, implement_module, show_screen_sessions, start_screen_session
 
 def parse_args():
     """Parse command line arguments."""
@@ -22,35 +22,41 @@ def main() -> None:
     
     if not os.path.isfile(args.path):
         raise RuntimeError(
-            f"no such file {args.path!r}"
+            f"\033[1;31m no such file {args.path!r} \033[0m"
         )
 
     config = configparser.ConfigParser()
     config.read(args.path)
 
+    args.board_type = config.get("device","name")
     args.build = config.get("menu","build")
     args.ipbb_dir = config.get("firmware","buildarea")
     args.project_type = config.get("firmware","type")
     args.vivado = config.get("vivado","version")
     module_id = args.mod_id
     module_name = f"module_{module_id}"
-    proj_path = os.path.join(args.ipbb_dir, "proj")
+    module_path = os.path.join(args.ipbb_dir, "proj", module_name)
 
     # Check for UGT_VIVADO_BASE_DIR
     args.vivado_base_dir = os.getenv("UGT_VIVADO_BASE_DIR")
     if not args.vivado_base_dir:
-        raise RuntimeError("Environment variable 'UGT_VIVADO_BASE_DIR' not set. Set with: 'export UGT_VIVADO_BASE_DIR=...'")
+        raise RuntimeError("\033[1;31m Environment variable 'UGT_VIVADO_BASE_DIR' not set. Set with: 'export UGT_VIVADO_BASE_DIR=...' \033[0m")
 
     # Vivado settings
     args.settings64 = os.path.join(args.vivado_base_dir, args.vivado, "settings64.sh")
     if not os.path.isfile(args.settings64):
         raise RuntimeError(
-            f"no such Xilinx Vivado settings file {args.settings64!r}\n"
-            f"  check if Xilinx Vivado {args.vivado} is installed on this machine."
+            f"\033[1;31m no such Xilinx Vivado settings file {args.settings64!r}\n \033[0m"
+            f"   \033[1;31m check if Xilinx Vivado {args.vivado} is installed on this machine. \033[0m"
         )
 
-    if not os.path.exists(proj_path):
-        raise RuntimeError(f"Path {proj_path!r} does not exist")    
+    if os.path.exists(module_path):
+        subprocess.run(["rm", "-rf", module_path]).check_returncode()
+
+    logging.info("===========================================================================")
+    logging.info("creating IPBB project for module %s ...", module_id)
+
+    create_module(module_id, module_name, args)
 
     logging.info("===========================================================================")
     logging.info("running IPBB project, synthesis and implementation, creating bitfile for module %s ...", module_id)
